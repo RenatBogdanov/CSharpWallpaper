3676b094b8cc0b3a0bf0619ccc28c99e91d49d36
XiIceDragoniX <danil_rudenko_2005@bk.ru>
2026-04-03 18:45:08 +0300

    Update V2

--- CSharpWallpaper/Controllers/MainController.cs
+++ CSharpWallpaper/Controllers/MainController.cs
@@ -18,7 +18,71 @@ namespace CSharpWallpaper.Controllers
         [Route("/")]
         public IActionResult Main()
         {
-            return View();
+            var mainViewModel = new WallpaperCollectionViewModel
+            {
+                // Вариант 1: Простые карточки
+                SimpleCards = new List<SimpleImageCardViewModel>
+            {
+                new SimpleImageCardViewModel
+                {
+                    ImageUrl = "/images/wallpapers/japan.jpg",
+                    ClickUrl = "/wallpapers/1"
+                },
+                new SimpleImageCardViewModel
+                {
+                    ImageUrl = "/images/wallpapers/japan.jpg",
+                    ClickUrl = "/wallpapers/2"
+                }
+            },
+                // TODO: Добавить переход по ссылке этому блоку
+                // Вариант 2: Картинка + текст
+                ImageTextCards = new List<ImageTextCardViewModel>
+            {
+                new ImageTextCardViewModel
+                {
+                    ImageUrl = "/images/wallpapers/japan.jpg",
+                    AltText = "Природа",
+                    Title = "Природа",
+                    Description = "Красивые пейзажи и природа",
+                    ClickUrl = "/wallpapers/1"
+                },
+                new ImageTextCardViewModel
+                {
+                    ImageUrl = "/images/wallpapers/japan.jpg",
+                    AltText = "Техника",
+                    Title = "Техника",
+                    Description = "Машины, самолеты, корабли",
+                    ClickUrl = "/wallpapers/2"
+                }
+            },
+
+                // Вариант 3: Картинка + иконка + текст (как Ирландия)
+                IconCards = new List<ImageIconTextCardViewModel>
+            {
+                new ImageIconTextCardViewModel
+                {
+                    ImageUrl = "/images/wallpapers/ireland.jpg",
+                    AltText = "Ирландия",
+                    IconUrl = "/images/flags/flagIreland.jpg", // или "/icons/ireland.svg"
+                    Title = "Ирландия",
+                    Description = "Красивые пейзажи Ирландии",
+                    ButtonText = "Смотреть коллекцию",
+                    ButtonUrl = "/collections/ireland"
+                },
+                new ImageIconTextCardViewModel
+                {
+                    ImageUrl = "/images/wallpapers/scotland.jpg",
+                    AltText = "Шотландия",
+                    IconUrl = "/images/flags/flagScotland.png",
+                    Title = "Шотландия",
+                    Description = "Горы и озера Шотландии",
+                    ButtonText = "Смотреть коллекцию",
+                    ButtonUrl = "/collections/scotland"
+                }
+            }
+            };
+
+            return View(mainViewModel);
         }
 
         [HttpGet("Privacy")]
